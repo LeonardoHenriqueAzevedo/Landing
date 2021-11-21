f36e70efa0f07584393b25100d4ccd15800495d3
Leonardo Henrique de Azevedo <leonardohenriquedeazevedo@gmail.com>
2021-11-21 03:31:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ function imgSlider(img) {
     document.querySelector('.starbucksImg').src = img;
 };
 
+// Put the circle in the image color
 function changeCircleColor(imgColor) {
     const circle = document.querySelector('.circle');
     circle.style.background = imgColor;
 }
-
