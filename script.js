893448d5450191630d4c7470120411c8b6dccaad
Leonardo Henrique de Azevedo <leonardohenriquedeazevedo@gmail.com>
2021-11-21 03:26:29 -0300

    code

---
+++ script.js
@@ -0,0 +1,9 @@
+// Put the image that we catch on onclick and put in .starbucksImg src
+function imgSlider(img) {
+    document.querySelector('.starbucksImg').src = img;
+};
+
+function changeCircleColor(a) {
+
+}
+
